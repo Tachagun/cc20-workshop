93e56144efe93404a2ded6624cf71577b2266295
Tachagun <techapat.kae@gmail.com>
2025-05-16 09:21:12 +0700

    starter with tailwind/daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+ 
+
+  return (
+    <div className='m-3'>
+      <h1 className="text-3xl ">Codecamp20</h1>
+      <button className="btn btn-primary">abc</button>
+    </div>
+  )
+}
+
+export default App
